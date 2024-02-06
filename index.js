f579c6bdde6c2362107ae9cbf0b8e991a62f366b
stylishsehar <stylishsehar262@gmail.com>
2024-02-06 17:12:02 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+// math expresion
+// no:1
+
+// no:2
+var num = 20%6;
+console.log(num);
+
+// no:3
+var largeNum =1000*2000;
+console.log(largeNum);
+
+// no:4
+var e = 8;
+var s = 13
+console.log(e-s);
+
+
+// no:6
+var numone =2*5
+alert(numone)
+
+// math expression 2
+
+// no:1
+var x = 10
+x=x+1
+console.log(x);
+
+// no:3
+var x=50
+var y =x+1
+console.log(y);
+
+// no:4
+var y =50
+var z = y-1
+console.log(z);
+
+// no:5
+var num = 35
+var newNum =num-1
+console.log(newNum);
+
+// no:6
+
+// no:7
+var f  = 12
+var e = f+1
+ alert(e)
+
+//  chapter 7
+// no:1
+var calnum = 2+(2*6)
+console.log(calnum);
+
+// no:2
+var calculatednum = (2+2)*6
+console.log(calculatednum);
+
+// no:3
+var number = (2+2)*(4+2)
+console.log(number);
+
+// no:4
+var num = ((2 + 2) * 4) + 2;
+console.log(num);
+
+// no:5
+var n =  8 + 2 * 4 + 10*4
+console.log(n);
+
+// no:6
+var a =  2 + 2 * 4 + 10
+console.log(a); 
+
+// no:7
+var f =  4 / 2 * 3-1
+console.log(f);
+
+
+
+
+
+
+
+ 
